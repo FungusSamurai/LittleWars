c117579634f4708388e5568104442e7b7a5b455b
FungusSamurai <brandon.tmacauley@gmail.com>
2018-01-17 18:20:09 -0500

    First Push!

---
+++ Little_Wars_V1.5/Assets/Scripts/Art/Cloud.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour {    
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(transform.forward * 5 * Time.deltaTime);
+        if (transform.position.z >= 400)
+            transform.position = new Vector3(transform.position.x, transform.position.y, -400);
+	}
+}
